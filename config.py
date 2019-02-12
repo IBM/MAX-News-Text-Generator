51b0f535d089763b4ba221b9f9e6e4abf8138d91
Brendan Dwyer <brendan.dwyer@ibm.com>
2019-02-12 13:16:02 -0800

    Fixed API_TITLE in config

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ RESTPLUS_MASK_SWAGGER = False
 SWAGGER_UI_DOC_EXPANSION = 'none'
 
 # API metadata
-API_TITLE = 'Model Asset Exchange Server'
+API_TITLE = 'Model Asset Exchange Microservice'
 API_DESC = 'An API for serving models'
 API_VERSION = '0.1'
