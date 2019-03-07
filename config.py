45f91924a4454b410cab854274e4ef34e781f07c
Nick Pentreath <nickp@za.ibm.com>
2019-03-07 16:24:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ SWAGGER_UI_DOC_EXPANSION = 'none'
 # API metadata
 API_TITLE = 'MAX News Text Generator'
 API_DESC = 'Generate English-language text similar to the news articles in the One Billion Words data set.'
-API_VERSION = '0.1'
+API_VERSION = '1.1.0'
