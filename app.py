5dfcbfc5c5502bbe4587368467c65c83258e88d3
Brendan Dwyer <brendan.dwyer@ibm.com>
2019-08-13 14:54:26 -0700

    add warning to app.py

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ from maxfw.core import MAXApp
 from api import ModelMetadataAPI, ModelPredictAPI
 from config import API_TITLE, API_DESC, API_VERSION
 
+print("NOTE: The minimum recommended resources for this model is 8 GB Memory and 4 CPUs")
+
 max_app = MAXApp(API_TITLE, API_DESC, API_VERSION)
 max_app.add_api(ModelMetadataAPI, '/metadata')
 max_app.add_api(ModelPredictAPI, '/predict')
